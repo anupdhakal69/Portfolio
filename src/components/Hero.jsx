2e62a838ec62bbc15a85c1c736a0776aac41c356
Anup D <dhakalanup96@gmail.com>
2024-09-25 10:08:38 +0545

    px-2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
 
-    <div className='px-6 mt-8 max-w-[400px] flex flex-col gap-5 items-center sm:max-w-[920px]  mx-auto h-full'>
+    <div className='px-2 mt-8 max-w-[400px] flex flex-col gap-5 items-center sm:max-w-[920px]  mx-auto h-full'>
 
         <img src="anup.jpg" alt="" className='h-48 sm:h-60 w-48 sm:w-60 object-cover rounded-full'/>
 
@@ -13,7 +13,7 @@ const Hero = () => {
         </div>
 
         <div className='flex gap-5'>
-          <button className='py-2 px-7 tracking-wide font-medium border rounded-full bg-gradient-to-r from-teal-500 to-sky-500 text-lg text-white'>Hire Me</button>
+          <button className='py-2 px-7 tracking-wide font-medium border-2 border-black rounded-full bg-gradient-to-r from-teal-500 to-sky-500 text-lg text-white'>Hire Me</button>
           <button className='py-2 px-7 tracking-wide font-medium border-2 border-gray-600 rounded-full text-lg hover:border-teal-600'>My resume</button>
         </div>
     </div>
