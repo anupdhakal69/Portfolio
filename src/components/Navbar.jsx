6330fc3b1652e397c7b9143017051e55052331ed
Anup D <dhakalanup96@gmail.com>
2024-09-25 06:33:37 +0545

    cross icon position changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
        {/* sidebar menu for mobile screens */}
         <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-gray-300 transition-all ${showMenu ? 'w-1/2':'w-0'}`}>
             <div className='flex flex-col  font-medium'>
-                <IoClose className='text-3xl cursor-pointer my-4 ml-4' onClick={toggleMenu}/>
+                <IoClose className='text-3xl cursor-pointer my-4 ml-32' onClick={toggleMenu}/>
                 <NavLink onClick={toggleMenu} className="py-3 pl-6 text-xl" to="/" >Home</NavLink>
                 <NavLink onClick={toggleMenu} className="py-3 pl-6 text-xl" to="/about" >About</NavLink>
                 <NavLink onClick={toggleMenu} className="py-3 pl-6 text-xl" to="/skills" >Skills</NavLink>
